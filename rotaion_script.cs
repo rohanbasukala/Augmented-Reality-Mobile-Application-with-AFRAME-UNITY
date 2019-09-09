fd6622b3d1c6d649a41ea00aed4e9b5e41122335
rohanbasukala <40158406+rohanbasukala@users.noreply.github.com>
2019-09-09 12:13:26 +0545

    Add files via upload

---
+++ rotaion_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class rotaion_script : MonoBehaviour, IVirtualButtonEventHandler {
+
+	public GameObject rotBtn;
+	public Animator houseAni;
+
+
+	// Use this for initialization
+	void Start () {
+		rotBtn = GameObject.Find ("ROTATION");
+		rotBtn.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		houseAni.GetComponent<Animator> ();
+		
+		
+	}
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+
+		houseAni.Play ("houseanimation");
+		Debug.Log ("BTN Pressed");
+
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+
+		houseAni.Play ("none");
+		Debug.Log ("BTN Released");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
